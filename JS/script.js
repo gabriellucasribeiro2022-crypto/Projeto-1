f74282c12e5b543e9dafebad2eea4ac981940484
gabriellucasribeiro2022-crypto <gabriellucas.ribeiro.22@gmail.com>
2026-04-15 14:19:14 -0300

    Retoques finais

--- JS/script.js
+++ JS/script.js
@@ -59,3 +59,54 @@ if (btnTema) {
         }
     });
 }
+
+/* Variaveis DOM */
+
+const charNome = "Kaelen, o Vingador das Presas";
+const charIdade = "32 Ciclos de Lua";
+const charFaccao = "Exilado (Ex-Guarda de Prata)";
+const charImportancia = "Último Sobrevivente do Massacre";
+const charHistoria = "Guerreiro canino motivado por um ódio profundo contra a seita dos Gatos Negros. Ele busca vingança pelo seu clã e a verdade sobre os planos sombrios dos felinos.";
+
+function carregarDadosPersonagem() {
+    if (document.getElementById('char-nome')) {
+        document.getElementById('char-nome').innerText = charNome;
+        document.getElementById('char-idade').innerText = charIdade;
+        document.getElementById('char-faccao').innerText = charFaccao;
+        document.getElementById('char-importancia').innerText = charImportancia;
+        document.getElementById('char-historia').innerText = charHistoria;
+    }
+}
+
+/* Ano de lançamento */
+
+window.onload = function() {
+    const naHome = window.location.pathname.endsWith("index.html") || window.location.pathname === "/";
+
+    if (naHome) {
+        const anoLancamentoConst = 2026; 
+        const anoAtualComputador = new Date().getFullYear();
+
+        if (anoAtualComputador === anoLancamentoConst) {
+            alert("🚀 GRANDE LANÇAMENTO: Fang & Steel já está entre nós!");
+        }
+    }
+
+    if (window.location.pathname.includes("personagens.html")) {
+        verificarAcessoPersonagens();
+        carregarDadosPersonagem(); 
+    }
+};
+
+function gerarSaudacao() {
+    const nomeUsuario = document.getElementById('input-nome').value;
+    const campoMensagem = document.getElementById('mensagem-boas-vindas');
+
+    if (nomeUsuario.trim() !== "") {
+        campoMensagem.innerText = `Saudações, ${nomeUsuario}! Sua jornada em Fang & Steel começa agora.`;
+        
+        campoMensagem.classList.add('estilo-saudacao'); 
+    } else {
+        alert("Por favor, digite seu nome para receber as honrarias do reino!");
+    }
+}
